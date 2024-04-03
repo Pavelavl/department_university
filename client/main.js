93a79e2705c95108d41018fc27a97c37b01ce5ab
Pavelavl <sfdrus2004@gmail.com>
2024-04-03 23:29:23 +0300

    added fetch

--- client/main.js
+++ client/main.js
@@ -1,17 +1,46 @@
 const port = 25522;
-const host = "127.0.0.1";
+const host = "192.168.0.100";
 const api = `http://${host}:${port}`;
+const gets = [
+  "departments",
+  "courses",
+  "students",
+  "subjects",
+  "groups",
+  "teachers",
+  "classrooms",
+  "curriculums",
+  "foreign_languages",
+  "languages",
+];
+const getsDict = gets.reduce((acc, el) => {
+  acc[el] = [];
+  return acc;
+}, {});
 
-post();
+init();
 
 function post(url, body) {
-  return fetch(url, {
+  return fetch(`${api}${url}`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: body,
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
   })
     .then((response) => response.json())
-    .then((body) => {
-      console.log(body);
+    .then((data) => {
+      console.log(data);
     });
+}
+
+function init() {
+  gets.forEach((tableName) => {
+    post("/get_data", {
+      query: `SELECT * FROM ${tableName};`,
+    }).then((data) => {
+      getsDict[tableName] = data;
+    });
+  });
 }
